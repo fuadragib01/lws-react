907bbebdde727e346c7ce185e46bdd6c02e86fef
Faad Ragib <ragibfuad@gmail.com>
2022-10-07 17:04:47 +0600

    till L-10 done,all about controlled input

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,23 +3,69 @@ import React, { Component } from "react";
 class Form extends Component {
 	state = {
 		title: "JavaScript",
+		text: "Javascript is awesome",
+		library: "react",
+		isAwesome: true,
 	};
 	changeHandler = (e) => {
-		this.setState({
-			title: e.target.value,
-		});
+		if (e.target.type === "text") {
+			this.setState({
+				title: e.target.value,
+			});
+		} else if (e.target.type === "textarea") {
+			this.setState({
+				text: e.target.value,
+			});
+		} else if (e.target.type === "select-one") {
+			this.setState({
+				library: e.target.value,
+			});
+		} else if (e.target.type === "checkbox") {
+			this.setState({
+				isAwesome: e.target.checked,
+			});
+		}
+	};
+	submitHandler = (e) => {
+		const { title, text, library, isAwesome } = this.state;
+		e.preventDefault();
+		console.log(title, text, library, isAwesome);
 	};
 	render() {
-		const { title } = this.state;
+		const { title, text, library, isAwesome } = this.state;
 		return (
 			<div>
-				<form>
+				<form onSubmit={this.submitHandler}>
 					<input
 						type="text"
 						placeholder="Enter title"
 						value={title}
 						onChange={this.changeHandler}
 					/>
+					<br />
+					<br />
+					<textarea
+						name="text"
+						value={text}
+						onChange={this.changeHandler}
+					/>
+					<br />
+					<br />
+					<select value={library} onChange={this.changeHandler}>
+						<option>Libraries</option>
+						<option value="react">React</option>
+						<option value="angular">Angular</option>
+					</select>
+					<br />
+					<br />
+					<input
+						type="checkbox"
+						onChange={this.changeHandler}
+						checked={isAwesome}
+					/>
+					<br />
+					<br />
+					<input type="submit" value="Submit" />
 				</form>
 			</div>
 		);
